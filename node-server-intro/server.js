2a309b3c57d1653688cac3c4c5b094240e9fd218
flare-s <webflarez@gmail.com>
2022-09-18 16:27:48 +0100

    Added a new route

--- node-server-intro/server.js
+++ node-server-intro/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 
 const server = express();
+
 server.get("/", (req, res) => {
   res.send("hello");
+});
+
+server.get("/uh-oh", (request, response) => {
+  response.status(500).send("something went wrong");
 });
 
 module.exports = server;
