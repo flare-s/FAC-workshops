53eed38092531fe8a8967efd3442f89c00c30651
flare-s <webflarez@gmail.com>
2022-09-25 20:45:19 +0100

    Deleting a task

--- learn-database/model/tasks.js
+++ learn-database/model/tasks.js
@@ -1,12 +1,14 @@
 const db = require("../database/db");
 
 const insert_task = db.prepare(
+  /*sql */
   `INSERT INTO tasks (content, complete)
    VALUES ($content, $complete)
    RETURNING id, content, created_at, complete`
 );
 
 const select_tasks = db.prepare(
+  /*sql */
   `SELECT 
     id, 
     content, 
@@ -15,15 +17,23 @@ const select_tasks = db.prepare(
   FROM tasks`
 );
 
+const delete_task = db.prepare(
+  /*sql*/
+  `DELETE FROM tasks WHERE id = ?`
+);
+
+const deleteTask = (id) => delete_task.run(id);
+
 const listTasks = () => select_tasks.all();
 
-const createTask = (task) => {
-  insert_task.get(task);
-};
+const createTask = (task) => insert_task.get(task);
 
 // Ex of adding a task
 createTask({ content: "Eat breakfast", complete: 1 });
 createTask({ content: "Go for a run", complete: 0 });
+
+deleteTask(1);
+
 console.log(listTasks());
 
 module.exports = { createTask, listTasks };
