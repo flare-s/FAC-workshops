7a9a8962fedea012c385378d467429264a16e205
flare-s <webflarez@gmail.com>
2022-09-25 16:23:49 +0100

    Amend the query and function to return the added task

--- learn-database/model/tasks.js
+++ learn-database/model/tasks.js
@@ -1,14 +1,17 @@
 const db = require("../database/db");
 
-const insert_task = db.prepare("INSERT INTO tasks (content) VALUES (?)");
+const insert_task = db.prepare(
+  "INSERT INTO tasks (content) VALUES (?) RETURNING id, content, created_at"
+);
 
 const createTask = (taskContent) => {
-  insert_task.run(taskContent);
+  insert_task.get(taskContent);
 };
 
 // Ex of adding a task
-// createTask("Eat an apple");
-// const tasks = db.prepare("SELECT * FROM tasks").all();
-// console.log(tasks);
+createTask("Eat breakfast");
+createTask("Go for a run");
+const tasks = db.prepare("SELECT * FROM tasks").all();
+console.log(tasks);
 
 module.exports = { createTask };
