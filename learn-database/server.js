de3ea78c75b1b40923ba4b3b6b50b093357f618c
flare-s <webflarez@gmail.com>
2022-09-26 13:24:50 +0100

    Showing the tasks on the UI

--- learn-database/server.js
+++ learn-database/server.js
@@ -3,12 +3,14 @@ const model = require("./model/tasks");
 const server = express();
 
 server.get("/", (req, res) => {
+  const tasks = model.selectTasks();
   const body = /*html*/ `
     <!doctype html>
     <form method="POST">
       <input id="content" name="content" aria-label="New task" required>
       <button>Add task +</button>
     </form>
+    <ul>${tasks.map((t) => `<li>${t.content}</li>`).join("")}</ul>
   `;
   res.send(body);
 });
