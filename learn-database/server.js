01a3a0c77ccc0aafabce88525bb15c60749f041d
flare-s <webflarez@gmail.com>
2022-09-26 13:19:26 +0100

    Creating the UI and adding the ability to add tasks to the db

--- learn-database/server.js
+++ learn-database/server.js
@@ -1,9 +1,25 @@
 const express = require("express");
-
+const model = require("./model/tasks");
 const server = express();
 
 server.get("/", (req, res) => {
-  res.send("hello world");
+  const body = /*html*/ `
+    <!doctype html>
+    <form method="POST">
+      <input id="content" name="content" aria-label="New task" required>
+      <button>Add task +</button>
+    </form>
+  `;
+  res.send(body);
+});
+
+server.post("/", express.urlencoded({ extended: false }), (req, res) => {
+  const task = {
+    content: req.body.content,
+    complete: 0,
+  };
+  model.insertTask(task);
+  res.redirect("/");
 });
 
 module.exports = server;
